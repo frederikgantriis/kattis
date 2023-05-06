cadfb314c3fec7bd13bf144b01c1eccdece4d7c7
frederikgantriis <frmo24042@gmail.com>
2023-05-06 10:45:44 +0200

    fixed texture analysis

--- texture-analysis/main.py
+++ texture-analysis/main.py
@@ -1,9 +1,15 @@
 import sys
 
+counter = 0
+
 while True:
     line = input()
 
+    counter += 1
+
     toNext = False
+    first = True
+    second = True
 
     whitePixels, blackPixels, prevspace, space = 0, 0, 0, 0
 
@@ -11,11 +17,33 @@ while True:
         sys.exit()
 
     for i in line:
+        
+
         if i == "*":
             blackPixels += 1
 
-            if prevspace != 0 and prevspace != space:
-                print("NOT EVEN")
+
+            if prevspace == 0:
+                if first:
+                    first = False
+
+                    prevspace = space
+                    space = 0
+                    continue
+                elif second:
+                    second = False
+
+                    prevspace = space
+                    space = 0
+                else:
+                    if prevspace != space:
+                        print(f"{counter} NOT EVEN")
+                        toNext = True
+                        break
+                continue
+
+            if prevspace != space:
+                print(f"{counter} NOT EVEN")
                 toNext = True
                 break
                 
@@ -27,4 +55,4 @@ while True:
             space += 1
         
     if toNext == False:
-        print("EVEN")
+        print(f"{counter} EVEN")
